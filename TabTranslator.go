337a0dc6d8370a6ce346f1e12c7cb238d6952232
Smurf <sudaru2@gmail.com>
2020-04-27 10:27:19 +0300

    must work

---
+++ TabTranslator.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	ResultTab  = "ResultTab"
+	AdvTimeTab = "AdvTimeTab"
+)
+
+var restabnew = []string{"Amount of edges", "Amount of vertex", "Path to graph", "Path to result", "time", "result", "mark"}
+var restabold = []string{"Amount of edges", "Amount of vertex", "Path to graph", "Path to result", "time", "result"}
+var advtime = []string{"mark time", "overall"}
+
+func main() {
+	walkDir := os.Args[1]
+
+	if err := filepath.Walk(walkDir, checkFile); err != nil {
+		log.Panic(err)
+	}
+}
+
+func checkFile(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+	switch info.Name() {
+	case ResultTab:
+		translateToCsv(path, false)
+	case AdvTimeTab:
+		translateToCsv(path, true)
+	default:
+		return nil
+	}
+
+	return nil
+}
+
+func translateToCsv(path string, isTime bool) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	csvtab := make([][]string, 1)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		tab := strings.Fields(scanner.Text())
+		csvtab = append(csvtab, tab)
+	}
+
+	if isTime {
+		csvtab[0] = advtime
+	} else {
+		if len(csvtab[1]) == 7 {
+			csvtab[0] = restabnew
+		} else {
+			csvtab[0] = restabold
+		}
+	}
+
+	newFile, err := os.Create(path + ".csv")
+	if err != nil {
+		return err
+	}
+	defer newFile.Close()
+
+	writer := csv.NewWriter(newFile)
+	writer.WriteAll(csvtab)
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/Rakiiii/goTabTranslator
+
+go 1.13
+
+require (
+	github.com/Rakiiii/goBoolMatrix v0.4.1 // indirect
+	github.com/Rakiiii/goGraph v0.3.2 // indirect
+	github.com/Rakiiii/goPair v0.0.3 // indirect
+	gonum.org/v1/plot v0.7.0 // indirect
+)
